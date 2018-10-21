9612e70b978ec223bcf7af0fd861cd4e536e845f
Anssi Salomaa <anssi.20@hotmail.com>
2018-10-22 00:09:16 +0300

    added daily stock data

--- crsp.R
+++ crsp.R
@@ -2,9 +2,10 @@ library(tidyverse)
 library(readr)
 library(lubridate)
 
-thirteenf <- read_rds("13f_output.rds")
+# reading CRSP monthly stock file
 
 crsp_monthly_stock_raw <- read_tsv("crsp_monthly_stock_file.txt", col_types = cols(.default = "c"))
+
 crsp_monthly_stock <- crsp_monthly_stock_raw %>% 
   transmute(permno = PERMNO,
             date = ymd(date),
@@ -14,10 +15,35 @@ crsp_monthly_stock <- crsp_monthly_stock_raw %>%
             cusip = CUSIP,
             price = abs(as.numeric(PRC)),
             shares_outstanding = as.numeric(SHROUT), # in 1000s
-            #return = if_else(as.numeric(RET) == c(-66.0, -77.0, -88.0, -99.0), NA, as.numeric(RET)),
-            trading_volume = case_when(VOL == "-99" ~ NA,
-                                       TRUE ~ as.numeric(VOL)))
+            return = as.numeric(RET), # e.g. value "C" is converted to NA
+            trading_volume = as.numeric(VOL))
 
-crsp_monthly_stock_raw %>% 
-  group_by(RET) %>% 
+crsp_monthly_stock %>% 
+  group_by(permno) %>% 
   tally()
+
+# Reading CRSP daily stock file
+
+crsp_daily_stock_raw <- read_tsv("crsp_daily_stock_file.txt", col_types = cols(.default = "c"))
+
+crsp_daily_stock_raw
+
+crsp_daily_stock <- crsp_daily_stock_raw %>% 
+  transmute(permno = PERMNO,
+            date = ymd(date),
+            ncusip = NCUSIP,
+            official_ticker = TICKER,
+            firm_crsp = COMNAM,
+            cusip = CUSIP,
+            price = abs(as.numeric(PRC)),
+            shares_outstanding = as.numeric(SHROUT), # in 1000s
+            return = as.numeric(RET), # e.g. value "C" is converted to NA
+            trading_volume = as.numeric(VOL))
+
+crsp_daily_stock
+
+crsp_monthly_stock %>% 
+  group_by(permno) %>% 
+  tally()
+
+write_rds(crsp_daily_stock, "crsp_daily_stock.rds")
