371282d9c8212b692ac1301b718e9798752a9791
Anssi Salomaa <anssi.20@hotmail.com>
2018-10-22 21:09:33 +0300

    adding full join for linking pin measures

--- information_asymmetry_measures.R
+++ information_asymmetry_measures.R
@@ -9,9 +9,10 @@ pin_dy <- pin_dy_raw %>%
   transmute(permno = permno,
             year = as.numeric(year),
             exchange = as.numeric(exchange), # NYSE or AMEX probably, 1=NYSE?, 2=AMEX?
-            pin = as.numeric(pin),
-            adj_pin = as.numeric(adjpin),
-            psos = as.numeric(psos))
+            pin_dy = as.numeric(pin),
+            pin_dy_adj = as.numeric(adjpin),
+            psos_dy = as.numeric(psos)) %>% 
+  select(-exchange)
 
 pin_dy %>% 
   group_by(permno) %>% 
@@ -23,7 +24,7 @@ pin_eho_raw <- read_delim("pin_eho_easley_hvidkjaer_o'hara.dat", delim = " ", co
 pin_eho <- pin_eho_raw %>% 
   transmute(permno = permn,
             year = as.numeric(year),
-            pin = as.numeric(pin))
+            pin_eho = as.numeric(pin))
 
 pin_eho %>%
   group_by(permno) %>% 
@@ -31,12 +32,21 @@ pin_eho %>%
 
 # PINs by Brown, Hillegeist and Lo (2004), PIN_BHL
 
-pin_bhl_raw <- read_delim("pin_bhl_brown_hillegeist_lo.asc_", delim = " ")
+pin_bhl_raw <- read_table("pin_bhl_brown_hillegeist_lo.asc_", col_names = T, col_types = cols(.default = "c"))
 
+pin_bhl <- pin_bhl_raw %>% 
+  transmute(permno = permno,
+            year = as.numeric(year),
+            pin_bhl = as.numeric(pinesas))
 
 # PINs by Brown and Hillegeist (2007), PIN_BH
 
-pin_bh_raw <- read_delim("pin_bh_brown_hillegeist.asc_", delim = " ", col_types = cols(.default = "c"))
+pin_bh_raw <- read_table("pin_bh_brown_hillegeist.asc_", col_names = T, col_types = cols(.default = "c"))
+
+pin_bh <- pin_bh_raw %>% 
+  transmute(permno = permno,
+            year = as.numeric(year),
+            pin_bh = as.numeric(pin))
 
 # MIAs by Johnson and So (2017)
 
@@ -45,4 +55,14 @@ mia_raw <- read_csv("mia_johnson_so.csv", col_types = cols(.default = "c"))
 mia <- mia_raw %>% 
   transmute(permno = PERMNO,
             date = mdy(DATE),
-            mia = as.numeric(MIA))
+            mia = as.numeric(MIA),
+            year = year(mdy(DATE)))
+
+
+information_asymmetry_measures_temp1 <- full_join(pin_dy, pin_eho)
+
+information_asymmetry_measures_temp2 <- full_join(information_asymmetry_measures_temp1, pin_bhl)
+
+information_asymmetry_measures_temp3 <- full_join(information_asymmetry_measures_temp2, pin_bh)
+
+information_asymmetry_measures_temp4 <- full_join(information_asymmetry_measures_temp3, mia)
