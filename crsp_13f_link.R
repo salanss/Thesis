c3985ea8fe5032ddf3f7b941ce0db5253760f82f
Anssi Salomaa <anssi.20@hotmail.com>
2018-10-31 07:27:14 +0200

    renamed to linking

--- crsp_13f_link.R
+++
@@ -1,8 +0,0 @@
-thirteenf <- read_rds("13f_output.rds")
-crsp_daily_stock <- read_rds("crsp_daily_stock.rds")
-
-thirteenf
-crsp_daily_stock
-
-thirteenf_temp1 <-
-  left_join(thirteenf, crsp_daily_stock, by = c("cusip" = "ncusip", "report_date" = "date"))
