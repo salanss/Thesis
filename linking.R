a2b84aba1a6525df95532ce5dbb1d0121acb98c4
Anssi Salomaa <anssi.20@hotmail.com>
2019-01-08 23:54:12 +0200

    improved linking.

--- linking.R
+++ linking.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(readr)
 library(lubridate)
 
+treated_firms_ibes <- read_rds("data/treated_firms_ibes.rds")
+
 thirteenf <- read_rds("data/13f_output.rds") %>% 
   mutate(report_date = ceiling_date(report_date, unit = "month") - days(1)) %>% 
   select(-institutional_ownership_percentage, -foreign_institutional_ownership_percentage, 
@@ -15,7 +17,7 @@ crsp_monthly_stock <- read_rds("data/crsp_monthly_stock.rds")
 compustat_quarter <- read_rds("data/compustat_quarter_final.rds") %>% 
   mutate(datadate = ceiling_date(datadate, unit = "month") - days(1)) # sanity check
 
-treated_firms_ibes <- read_rds("data/treated_firms_ibes.rds")
+information_asymmetry_measures <- read_rds("data/information_asymmetry_measures.rds")
 
 thirteenf_crsp_merged <- thirteenf %>%
   inner_join(crsp_monthly_stock, by = c("cusip" = "ncusip", "report_date" = "date"))
@@ -23,14 +25,17 @@ thirteenf_crsp_merged <- thirteenf %>%
 thirteenf_crsp_compustat_quarter_merged <- thirteenf_crsp_merged %>% 
   inner_join(compustat_quarter, by = c("permno", "report_date" = "datadate")) %>% 
   transmute(report_date = report_date,
+            year = year(report_date),
             permno = permno,
             cusip = cusip,
-            shares_outstanding = shares_outstanding.y, #coalesce(shares_outstanding.x, shares_outstanding.y, shares_outstanding)
+            shares_outstanding = shares_outstanding.y, #crsp 
+                                              # coalesce(shares_outstanding.x, shares_outstanding.y, shares_outstanding)
             institutional_ownership_shares = institutional_ownership_shares,
             foreign_institutional_ownership_shares = foreign_institutional_ownership_shares,
             domestic_institutional_ownership_shares = domestic_institutional_ownership_shares,
             inst_percentage = institutional_ownership_shares / shares_outstanding,
             foreign_inst_percentage = foreign_institutional_ownership_shares / shares_outstanding,
+            domestic_inst_percentage = domestic_institutional_ownership_shares / shares_outstanding,
             sic_code = if_else(sic_code.x == "0" | is.na(sic_code.x) == T, sic_code.y, sic_code.x),
             price = price, # coalesce(price, price_close_calendar)
             market_cap = shares_outstanding * price,
@@ -42,27 +47,23 @@ thirteenf_crsp_compustat_quarter_merged <- thirteenf_crsp_merged %>%
             tobin_q = tobin_q) %>% 
   arrange(report_date, permno)
 
-thirteenf_crsp_merged <- read_rds("data/thirteenf_crsp_merged.rds") %>% 
-  transmute(report_date = report_date, 
-            year = year(report_date),
-            permno = permno, 
-            cusip = cusip,
-            inst_percentage = institutional_ownership_shares/shares_outstanding,
-            foreign_inst_percentage = foreign_institutional_ownership_shares/shares_outstanding,
-            domestic_inst_percentage = domestic_institutional_ownership_shares/shares_outstanding) %>% 
-  arrange(report_date)
-
-information_asymmetry_measures <- read_rds("data/information_asymmetry_measures.rds")
-
-thirteenf_crsp_ia_merged <- thirteenf_crsp_merged %>% 
-  group_by(permno, year) %>% 
+thirteenf_crsp_compustat_ia_merged <- thirteenf_crsp_compustat_quarter_merged %>% 
+  select(permno, year, sic_code, inst_percentage, foreign_inst_percentage, domestic_inst_percentage,
+         market_cap, log_market_cap, book_equity, book_to_market, leverage, roa, tobin_q) %>% 
+  filter_all(all_vars(!is.na(.))) %>% # filter all NAs away, since going to be used in regression 
+  group_by(permno, year, sic_code) %>% 
   summarise(inst_percentage = mean(inst_percentage),
             foreign_inst_percentage = mean(foreign_inst_percentage),
-            domestic_inst_percentage = mean(domestic_inst_percentage)) %>% 
+            domestic_inst_percentage = mean(domestic_inst_percentage),
+            log_market_cap = log(mean(market_cap)),
+            book_to_market = mean(book_to_market),
+            leverage = mean(leverage),
+            roa = mean(roa),
+            tobin_q = mean(tobin_q)) %>% 
   ungroup() %>% 
   inner_join(information_asymmetry_measures, by = c("permno", "year"))
 
-write_rds(thirteenf_crsp_ia_merged, "data/baseline_regression_raw.rds")
+write_rds(thirteenf_crsp_compustat_ia_merged, "data/baseline_regression_raw.rds")
 
 ##
 
