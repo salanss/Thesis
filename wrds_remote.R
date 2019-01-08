5b3d76731867f952dc45988642c913e811875970
Anssi Salomaa <anssi.20@hotmail.com>
2019-01-08 21:28:06 +0200

    testing

--- wrds_remote.R
+++ wrds_remote.R
@@ -17,7 +17,7 @@ data_all
 
 res <- dbSendQuery(wrds, "select distinct table_name
                    from information_schema.columns
-                   where table_schema='tfn'
+                   where table_schema='crsp'
                    order by table_name")
 tfn <- dbFetch(res, n=-1)
 dbClearResult(res)
@@ -25,21 +25,18 @@ tfn
 
 res <- dbSendQuery(wrds, "select column_name
                    from information_schema.columns
-                   where table_schema='tfn'
-                   and table_name='s34'
+                   where table_schema='crsp'
+                   and table_name='msf'
                    order by column_name")
 data <- dbFetch(res, n=-1)
 dbClearResult(res)
 data
 
-res <- dbSendQuery(wrds, "select rdate, 
-                                 cusip, 
-                                 sum(shares) as shares
-                          from tfn.s34
-                          where rdate between '1979-12-31'
-                          and '2017-01-01'
-                          group by cusip, rdate")
-data <- dbFetch(res, n=100)
+res <- dbSendQuery(wrds, "select *
+                          from crsp.msf
+                          where date between '1980-01-01'
+                          and '2017-01-01'")
+data <- dbFetch(res, n=-1)
 dbClearResult(res)
 library(tidyverse)
 data <- as.tbl(data)
