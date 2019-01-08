6b1e58472338ebbf37f74dfc6f46b91c33c579c6
Anssi Salomaa <anssi.20@hotmail.com>
2019-01-08 21:27:34 +0200

    small fixes

--- crsp.R
+++ crsp.R
@@ -8,32 +8,33 @@ crsp_monthly_stock_raw <- read_tsv("data/crsp_monthly_stock_file.txt", col_types
 
 crsp_monthly_stock <- crsp_monthly_stock_raw %>% 
   transmute(permno = PERMNO,
-            date = ymd(date),
+            date = ceiling_date(ymd(date), unit = "month") - days(1),
             ncusip = NCUSIP,
             official_ticker = TICKER,
             firm = COMNAM,
             sic_code = SICCD, # historical
             exchange_code = EXCHCD,
-            price = abs(parse_double(PRC)),
+            price = if_else(is.na(PRC), abs(parse_double(DLPRC)),
+                            abs(parse_double(PRC))),
             price_delisting = abs(parse_double(DLPRC)),
             bid = abs(parse_double(BIDLO)),
             ask = abs(parse_double(ASKHI)),
             bid_ask_spread = (ask - bid)/price,
             shares_outstanding = parse_double(SHROUT)*1000, # in 1000s
-            return = case_when(
-              RET == "C" ~ NA_real_,
-              TRUE ~ parse_double(RET)), # e.g. value "C" is converted to NA
+            return = if_else(is.na(RET), parse_double(DLRET),
+                             parse_double(RET)), # e.g. value "C" is converted to NA
             return_delisting = parse_double(DLRET),
             trading_volume = parse_double(VOL)) %>% 
-  select(-official_ticker, -firm)
+  select(-official_ticker, -firm) %>% 
+  filter(exchange_code %in% c("1", "2", "3")) %>% # keeping only NYSE, AMEX and Nasdaq
+  filter(!is.na(ncusip)) %>% 
+  distinct()
 
 write_rds(crsp_monthly_stock, "data/crsp_monthly_stock.rds")
 
 # Reading CRSP daily stock file
 
 crsp_daily_stock_raw <- read_tsv("crsp_daily_stock_file.txt", col_types = cols(.default = "c"))
-
-crsp_daily_stock_raw
 
 crsp_daily_stock <- crsp_daily_stock_raw %>% 
   transmute(permno = PERMNO,
