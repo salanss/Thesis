45e71be142c5d0ecdfeb88f058b96244a95151c1
Salomaa Anssi <ASAL@hansel.fi>
2019-01-10 15:33:38 +0200

    markuksen fixi stargazer-taulukkoon

--- regressions.R
+++ regressions.R
@@ -18,9 +18,21 @@ baseline_regression <- baseline_regression_raw %>%
   group_by(dep_measure_name, ia_measure_name) %>% 
   nest()
 
-model_function <- function(df) {
-  felm(dep_measure ~ ia_measure + log_market_cap + book_to_market + leverage +
-         roa + tobin_q|year + sic_code|0|year+sic_code, data = df)
+rnm <- function(df, ia_meas, dep_meas) {
+  rnm_list <- set_names(c("ia_measure", "dep_measure"), syms(c(ia_meas, dep_meas)))
+  rename(df, !!!rnm_list)
+}
+
+baseline_names <- mutate(baseline_regression,
+                         data_named = pmap(list(data, ia_measure_name, dep_measure_name), rnm),
+                         model = pmap(list(data_named, ia_measure_name, dep_measure_name), model_function))
+
+model_function <- function(df, ia_meas, dep_meas) {
+  f <- dep_measure ~ ia_measure + log_market_cap  + leverage +
+    roa + tobin_q|year + sic_code|0|year+sic_code
+  f[[2]] <- sym(dep_meas)
+  f[[3]][[2]][[2]][[2]][[2]][[2]][[2]][[2]] <- sym(ia_meas)
+  felm(f, data = df)
 }
 
 models <- baseline_regression %>%
@@ -29,25 +41,18 @@ models <- baseline_regression %>%
 models
 
 l <- models[[1]]
-  
-b_fun <- function(mod){
-  coefficients(mod)[[1]]
-}
 
-models %>% transmute(ia_measure_name,
-                     beta = map_dbl(model, b_fun))
-
-stargazer(l, title = "Baseline regression results (H1)", out = ".html")
+stargazer(baseline_names$model, title = "Baseline regression results (H1)", out = ".html")
 
 models_list <- models %>% 
   mutate(models_list = map2(data, models, list))
 
 
-lm_model <- felm(foreign_inst_percentage ~ pin_dy_adj + log_market_cap + book_to_market + 
+lm_model <- felm(foreign_inst_percentage ~ pin_dy_adj + log_market_cap  + 
                      leverage + roa + tobin_q, data = baseline_regression_raw)
 
 
-baseline_regression_raw %>% 
-  filter(!is.na(pin_bhl)) %>% 
-  filter(foreign_inst_percentage <= 1) %>% 
-  ggplot(aes(pin_bhl, foreign_inst_percentage)) + geom_point() + facet_wrap(~ year, nc = 3)
+lm_model2 <- felm(foreign_inst_percentage ~ pin_dy + log_market_cap  + 
+                   leverage + roa + tobin_q, data = baseline_regression_raw)
+
+stargazer(lm_model, lm_model2, title = "Baseline regression results (H1)", out = ".html")
