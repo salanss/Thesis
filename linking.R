2e0753da60ea047e182141ebd99595b0c571147e
Anssi Salomaa <anssi.20@hotmail.com>
2019-01-15 20:49:19 +0200

    did the same logic as in analyst coverage for all measures

--- linking.R
+++ linking.R
@@ -22,12 +22,13 @@ information_asymmetry_measures <- read_rds("data/information_asymmetry_measures.
 thirteenf_crsp_merged <- thirteenf %>%
   inner_join(crsp_monthly_stock, by = c("cusip" = "ncusip", "report_date" = "date"))
 
-thirteenf_crsp_compustat_quarter_merged <- thirteenf_crsp_merged %>% 
+thirteenf_crsp_compustat_merged <- thirteenf_crsp_merged %>% 
   inner_join(compustat_quarter, by = c("permno", "report_date" = "datadate")) %>% 
   transmute(report_date = report_date,
             year = year(report_date),
             permno = permno,
             cusip = cusip,
+            sic_code = if_else(sic_code.x == "0" | is.na(sic_code.x) == T, sic_code.y, sic_code.x),
             shares_outstanding = shares_outstanding.y, #crsp 
                                               # coalesce(shares_outstanding.x, shares_outstanding.y, shares_outstanding)
             institutional_ownership_shares = institutional_ownership_shares,
@@ -36,9 +37,8 @@ thirteenf_crsp_compustat_quarter_merged <- thirteenf_crsp_merged %>%
             inst_percentage = institutional_ownership_shares / shares_outstanding,
             foreign_inst_percentage = foreign_institutional_ownership_shares / shares_outstanding,
             domestic_inst_percentage = domestic_institutional_ownership_shares / shares_outstanding,
-            sic_code = if_else(sic_code.x == "0" | is.na(sic_code.x) == T, sic_code.y, sic_code.x),
             price = price, # coalesce(price, price_close_calendar)
-            market_cap = shares_outstanding * price,
+            market_cap = market_cap.x/1000000, # crsp to same units as compustat
             log_market_cap = log(market_cap),
             book_equity = book_equity,
             book_to_market = book_to_market,
@@ -47,7 +47,7 @@ thirteenf_crsp_compustat_quarter_merged <- thirteenf_crsp_merged %>%
             tobin_q = tobin_q) %>% 
   arrange(report_date, permno)
 
-thirteenf_crsp_compustat_ia_merged <- thirteenf_crsp_compustat_quarter_merged %>% 
+thirteenf_crsp_compustat_ia_merged <- thirteenf_crsp_compustat_merged %>% 
   select(permno, year, sic_code, inst_percentage, foreign_inst_percentage, domestic_inst_percentage,
          market_cap, log_market_cap, book_equity, book_to_market, leverage, roa, tobin_q) %>% 
   filter_all(all_vars(!is.na(.))) %>% # filter all NAs away, since going to be used in regression 
@@ -67,48 +67,53 @@ write_rds(thirteenf_crsp_compustat_ia_merged, "data/baseline_regression_raw.rds"
 
 ## did linking
 
+# first by id:s cusip (and filter NA permnos)
+
 ibes_did <- read_rds("data/ibes_did")
 
-crsp_monthly_stock <- read_rds("data/crsp_monthly_stock.rds") %>% 
-  mutate(year_quarter = quarter(date, with_year = T))
-  
-compustat_quarter <- read_rds("data/compustat_quarter_final.rds") %>% 
-  mutate(datadate = ceiling_date(datadate, unit = "month") - days(1)) # sanity check
-
-ibes_crsp_link1 <- ibes_did %>% 
-  inner_join(crsp_monthly_stock, by = c("cusip" = "ncusip", "measurement_year_quarter" = "year_quarter")) #%>% 
-  select(-date, -price, -shares_outstanding, -return, -trading_volume) %>%
+crsp_compustat_keys <- thirteenf_crsp_compustat_merged %>% 
+  select(permno, cusip, sic_code) %>% 
   distinct()
 
-ibes_crsp_link1 %>% 
-  group_by(permno) %>% 
-  tally()
+did_temp1 <- ibes_did %>% 
+  left_join(crsp_compustat_keys, by = "cusip") %>% 
+  filter(!is.na(permno))
 
-# link for 13F (only event_date and permno)
+# retrieve data from thirteenf_crsp_compustat-data and do the interval filtering [-15;-3] and [3;15] for all measures
+# and link that to ibes firms (treated, control, before and after events)
 
-ibes_crsp_link2 <- ibes_crsp_link1 %>% 
-  select(permno, event_date) %>% 
-  distinct()
+events <- read_rds("data/events.rds")
 
-thirteenf_ibes_link1 <- left_join(thirteenf_temp1, ibes_crsp_link2, by = "permno") %>% 
-  select(permno, report_date, foreign_institutional_ownership_percentage, event_date) %>%
-  filter(!is.na(event_date)) %>% 
-  mutate(yearbefore = report_date %within% interval(event_date %m-% months(15), event_date %m-% months(3) + days(3)),
-         yearafter = report_date %within% interval(event_date %m+% months(3) - days(3), event_date %m+% months(15))) %>% 
-  arrange(permno, report_date, event_date)
+columns_for_summarise <- c("inst_percentage", "foreign_inst_percentage", "domestic_inst_percentage", 
+                           "market_cap", "log_market_cap", "book_to_market", "leverage", "roa", "tobin_q")
 
-thirteenf_ibes_link2 <- thirteenf_ibes_link1 %>% 
-  filter(yearbefore == T) %>% 
-  group_by(permno, event_date) %>% 
-  summarise(foreign_holdings = mean(foreign_institutional_ownership_percentage)) %>% 
-  ungroup()
+filter1 <- function (df, interval){
+  filter(df, report_date %within% interval)
+}
 
-thirteenf_ibes_link3 <- thirteenf_ibes_link1 %>% 
-  filter(yearafter == T) %>% 
-  group_by(permno, event_date) %>% 
-  summarise(foreign_holdings = mean(foreign_institutional_ownership_percentage)) %>% 
-  ungroup()
+summarise_mean <- function (df) {
+  df %>% 
+    group_by(permno, event_date) %>% 
+    summarise_at(columns_for_summarise, mean) %>% 
+    ungroup()
+}
 
-crsp_monthly_stock %>% 
-  group_by(permno) %>% 
-  tally()
+before_val <- map2(events$before_interval,
+                   events$event_date,
+                   ~filter1(thirteenf_crsp_compustat_merged, .x) %>% mutate(event_date = .y)) %>% 
+  map_df(~summarise_mean(.x)) %>% 
+  mutate(after = 0)
+
+after_val <- map2(events$after_interval,
+                  events$event_date,
+                  ~filter1(thirteenf_crsp_compustat_merged, .x) %>% mutate(event_date = .y)) %>% 
+  map_df(~summarise_mean(.x)) %>% 
+  mutate(after = 1)
+
+vals <- bind_rows(before_val, after_val) %>% 
+  arrange(permno, event_date, after)
+
+did <- did_temp1 %>% 
+  inner_join(vals, by = c("permno", "event_date", "after"))
+
+write_rds(did, "data/did_regression_raw.rds")
