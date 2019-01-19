b70cb99fd7aa5969c18d4df93da5b3dcce9006bb
Anssi Salomaa <anssi.20@hotmail.com>
2019-01-19 18:34:43 +0200

    fixes

--- 13f_s34_master_file.R
+++ 13f_s34_master_file.R
@@ -6,47 +6,64 @@ library(feather)
 # reading 13f institutional holdings, stock ownership summary file, entire database, 1980-01, 2017-012
 
 
-# df_institutions_raw <- read_tsv("data/13f_institutions.txt", col_types = cols(.default = "c"))
-# 
-# df_institutions <- df_institutions_raw %>% 
-#   group_by(mgrno) %>% 
-#   fill(country, .direction = "up") %>% 
-#   fill(country, .direction = "down") %>% 
-#   ungroup() %>% 
-#   select(-mgrname, -permkey)
-# 
-# write_feather(df_institutions, "data/13_institutions.feather")
-# 
-# 
-# df_raw <- read_tsv("data/13f_s34_master_file.txt", col_types = cols(.default = "c")) %>% 
-#   select(-country)
-# 
-# write_feather(df_raw, "data/13_raw_master.feather")
+df_institutions_raw <- read_tsv("data/13f_institutions.txt", col_types = cols(.default = "c"))
 
-df_institutions <- read_feather("data/13_institutions.feather") %>% 
-  distinct() %>% 
-  arrange(rdate)
+df_institutions <- df_institutions_raw %>% 
+  transmute(file_date = ymd(fdate),
+            report_date = ymd(rdate),
+            country = country,
+            mgrno = mgrno,
+            manager = mgrname) %>% 
+  filter(report_date >= ymd(19971231)) %>% 
+  group_by(mgrno) %>% 
+  fill(country, .direction = "down") %>% 
+  fill(country, .direction = "up") %>% 
+  ungroup() %>% 
+  filter(!is.na(country))
 
-df <- read_feather("data/13_raw_master.feather") %>% 
+df_institutions_final <- df_institutions %>% 
+  group_by(mgrno, report_date, country) %>% 
+  summarise(file_date = min(file_date)) %>% 
+  ungroup()
+
+write_rds(df_institutions_final, "data/13f_institutions.rds")
+
+df_raw <- read_tsv("data/13f_holdings.txt", col_types = cols(.default = "c"))
+
+df_holdings <- df_raw %>% 
+  transmute(file_date = ymd(fdate),
+            cusip = cusip,
+            mgrno = mgrno,
+            shareholdings = parse_double(shares))
+
+
+write_feather(df_holdings, "data/13f_holdings.feather")
+
+
+df_institutions <- read_rds("data/13f_institutions.rds") %>% 
+  distinct()
+
+df <- read_feather("data/13f_holdings.feather") %>% 
   distinct()
 
 df_temp1 <- df %>% 
-  left_join(df_institutions)
+  left_join(df_institutions, by = c("mgrno", "file_date"))
 
 write_feather(df_temp1, "data/13f_masterfile_merged.feather")
 
 df_temp2 <- read_feather("data/13f_masterfile_merged.feather") %>% 
   transmute(cusip = cusip,
-            official_ticker = ticker,
-            firm_13f = stkname,
+            report_date = report_date,
+            file_date = file_date,
             institutional = mgrno,
             institutional_country = country,
-            institutional_type = typecode,
-            report_date = ymd(rdate),
-            shareholdings_end_qtr = parse_double(shares),
-            shares_outstanding_millions = parse_double(shrout1),
-            shares_outstanding_thousands = parse_double(shrout2),
-            market_price = parse_double(prc))
+            shareholdings_end_qtr = shareholdings)
+
+## crsp link
+
+crsp_quarter_stock <- read_rds("data/crsp_quarter_stock.rds") %>% 
+  select(permno, ncusip) %>% 
+  distinct()
 
 df_temp3 <- df_temp2 %>% 
   group_by(report_date, cusip) %>% 
