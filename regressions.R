7f54becff381d8333f942b24202d5afebeb29566
Anssi Salomaa <anssi.20@hotmail.com>
2019-01-22 19:39:14 +0200

    added breadth measures

--- regressions.R
+++ regressions.R
@@ -53,12 +53,21 @@ did_regression_raw <- read_rds("data/did_regression_raw.rds")
 
 summary(did_regression_raw)
 
+# log_market_cap = mean(log_market_cap, win_e.g. = [-15;-3]) or 
+# log_market_cap = log(mean(market_cap), win_e.g. = [-15;-3])
+
+columns_for_summarise <- c("inst_percentage", "foreign_inst_percentage", "domestic_inst_percentage", 
+                           "inst_breadth", "foreign_breadth", "domestic_breadth", 
+                           "market_cap", "book_to_market", "leverage", "roa", "tobin_q")
+
 did_regression <- did_regression_raw %>% 
-  filter_all(all_vars(!is.infinite(.))) %>% 
-  filter_all(all_vars(!is.na(.))) %>% 
+  filter(quarter_index %in% c(-12:12)) %>% 
+  group_by(permno, event_date, treated, after) %>% 
+  summarise_at(columns_for_summarise, mean) %>% 
+  ungroup() %>% 
   mutate(year = year(event_date),
-         log_market_cap = log(market_cap)) # log_market_cap = mean(log_market_cap, win_e.g. = [-15;-3]) or 
-                                           # log_market_cap = log(mean(market_cap), win_e.g. = [-15;-3])
+         log_market_cap = log(market_cap))
+  
 
 did_model1 <- felm(foreign_inst_percentage ~ treated + after + treated * after + log_market_cap + book_to_market + 
                      leverage + roa + tobin_q |year + permno|0|year+permno, data = did_regression)
@@ -66,32 +75,40 @@ did_model1 <- felm(foreign_inst_percentage ~ treated + after + treated * after +
 did_model2 <- felm(domestic_inst_percentage ~ treated + after + treated * after + log_market_cap + book_to_market + 
                      leverage + roa + tobin_q |year + permno|0|year+permno, data = did_regression)
 
-did_model3 <- felm(inst_percentage ~ treated + after + treated * after + log_market_cap + book_to_market + 
+did_model3 <- felm(foreign_breadth ~ treated + after + treated * after + log_market_cap + book_to_market + 
                      leverage + roa + tobin_q |year + permno|0|year+permno, data = did_regression)
 
-did_list <- list(did_model1, did_model2, did_model3)
+did_model4 <- felm(domestic_breadth ~ treated + after + treated * after + log_market_cap + book_to_market + 
+                     leverage + roa + tobin_q |year + permno|0|year+permno, data = did_regression)
+
+did_list <- list(did_model1, did_model2, did_model3, did_model4)
 
 stargazer(did_list, title = "Difference-in-differences regression results (H2)", out = "DiD H2 results.html")
 
-library(ggplot2)
-
-data <- did_regression %>% 
-  filter(foreign_inst_percentage <= 1 & foreign_inst_percentage >= 0) %>% 
+data <- did_regression_raw %>% 
   mutate(treated = if_else(treated == 1, "treated", "control")) %>% 
-  group_by(treated, year_index) %>% 
-  summarise(foreign_inst_percentage = mean(foreign_inst_percentage)) %>% 
+  group_by(treated, quarter_index) %>% 
+  summarise(foreign_inst_percentage = mean(foreign_inst_percentage),
+            foreign_breadth = mean(foreign_breadth),
+            domestic_inst_percentage = mean(domestic_inst_percentage),
+            domestic_breadth = mean(domestic_breadth)) %>% 
   ungroup()
 
-ggplot(data, aes(year_index, foreign_inst_percentage, group = treated, color = treated)) + 
+ggplot(data, aes(quarter_index, domestic_breadth, group = treated, color = treated)) + 
   geom_line() +
   geom_vline(xintercept=0) +
   theme_classic()
 
 d <- baseline_regression_raw %>% 
-  filter(inst_percentage <= 1 & inst_percentage >= 0) %>% 
   group_by(year) %>% 
-  summarise(inst_percentage = mean(inst_percentage))
+  summarise(inst_percentage = mean(inst_percentage),
+            foreign_inst_percentage = mean(foreign_inst_percentage),
+            domestic_inst_percentage = mean(domestic_inst_percentage))
 
-ggplot(d, aes(year, inst_percentage)) + geom_line() + theme_classic()
+ggplot(d, aes(year)) + 
+  #geom_line(aes(y = inst_percentage, colour = "inst_percentage")) + 
+  geom_line(aes(y = foreign_inst_percentage, colour = "foreign_inst_percentage")) +
+  #geom_line(aes(y = domestic_inst_percentage, colour = "domestic_inst_percentage")) + 
+  theme_classic()
 
        
